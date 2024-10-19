ec68abd34c6515ba83f9c1e0e0d1aa0748005601
Snow140 <pj542859@gmail.com>
2024-10-18 20:12:44 -0400

    Update mouse_hover.gd

--- scripts/mouse_hover.gd
+++ scripts/mouse_hover.gd
@@ -1,9 +1,19 @@
 extends Node2D
 
-func _ready() -> void:
-	pass # Replace with function body.
+func is_even(integer):
+	return integer % 2 == 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position = get_viewport().get_mouse_position()
+	var tileNode = get_parent().get_node("Level1/tileMap")
+	var mouse_cell_pos = tileNode.local_to_map( get_global_mouse_position() )
+	if is_even(mouse_cell_pos.x):
+		mouse_cell_pos.x -= 1
+	if not is_even(mouse_cell_pos.y):
+		mouse_cell_pos.y -= 1
+	self.global_position = tileNode.map_to_local(mouse_cell_pos) - Vector2(26,13)
+	var hide = true
+	$coll.global_position = get_global_mouse_position()
+	for obj in $coll.get_overlapping_bodies():
+		hide = false
+	if hide: visible = false
+	else: visible = true
