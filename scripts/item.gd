d6afa7cc83f62bf3ddaa222bbb0abc1a08323e2e
Snow140 <pj542859@gmail.com>
2024-12-02 09:13:30 -0500

    Update item.gd

--- scripts/item.gd
+++ scripts/item.gd
@@ -7,17 +7,16 @@ var type = ""
 func _ready() -> void:
 	if type == "bolt":
 		$sprite.texture = load("res://assets/bolt.png")
-	elif type == "strm":
+	if type == "strm":
 		$sprite.texture = load("res://assets/storm.png")
-		
-	hover.connect("mouse_entered", enlarge)
-	hover.connect("mouse_exited", shrink)
+	
 		
 func enlarge():
 	$sprite.scale = Vector2(1.2, 1.2)
-	if get_parent().get_parent().get_parent().hoveredTool == null:
-		get_parent().get_parent().get_parent().hoveredTool = self.type
+	if not get_parent().get_parent().get_parent().hoveredTool.has(self.type):
+		get_parent().get_parent().get_parent().hoveredTool.append(self.type)
 	
 func shrink():
+	print("EXIT")
 	$sprite.scale = Vector2(1, 1)
-	get_parent().get_parent().get_parent().hoveredTool = null
+	get_parent().get_parent().get_parent().hoveredTool.erase(self.type)
