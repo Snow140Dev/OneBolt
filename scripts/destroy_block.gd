e5b8112a676db9ed00013b6870bf7b1d54ce6f55
Snow140 <pj542859@gmail.com>
2024-12-03 09:17:34 -0500

    Add files via upload

--- scripts/destroy_block.gd
+++ scripts/destroy_block.gd
@@ -1,5 +1,10 @@
-extends Node
+extends Node2D
+
+@onready var lightningScene = preload("res://objects/lightning_bolt.tscn")
 
 func destroy(tilemap, coords):
 	tilemap.set_cell(1, coords, 0)
 	
+	var bolt = lightningScene.instantiate()
+	bolt.get_node("Line2D").points = PackedVector2Array([Vector2(get_global_mouse_position().x,-16*get_global_mouse_position().y), Vector2(get_global_mouse_position().x,get_global_mouse_position().y)])
+	get_parent().get_parent().add_child(bolt)
