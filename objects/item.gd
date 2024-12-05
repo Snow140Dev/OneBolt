946dc727c2194bfd5e0889d7da3e3d10c981c7c5
Snow140 <pj542859@gmail.com>
2024-12-05 09:15:48 -0500

    Add files via upload

--- objects/item.gd
+++ objects/item.gd
@@ -20,10 +20,16 @@ func _ready() -> void:
 func enlarge():
 	print("ENTER")
 	sprite.scale = Vector2(1.2, 1.2)
+	get_parent().get_parent().get_parent().showItemPopup(null, null, null)
 	if not get_parent().get_parent().get_parent().hoveredTool.has(self.type):
 		get_parent().get_parent().get_parent().hoveredTool.append(self.type)
+		get_parent().get_parent().get_parent().hoveredToolPos.append([self.global_position.x,self.global_position.y])
+		
 	
 func shrink():
 	print("EXIT")
 	sprite.scale = Vector2(1, 1)
 	get_parent().get_parent().get_parent().hoveredTool.erase(self.type)
+	get_parent().get_parent().get_parent().hoveredToolPos.erase([self.global_position.x,self.global_position.y])
+	
+	get_parent().get_parent().get_parent().hideItemPopup()
